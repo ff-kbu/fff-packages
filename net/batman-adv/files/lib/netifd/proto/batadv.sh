d87edec883b72a2a66f6d1cc5c6a8188f846c541
ramponee <rampone@gmail.com>
2014-02-06 21:46:40 +0100

    upgrade to 2013.4.0 from luebeck

--- net/batman-adv/files/lib/netifd/proto/batadv.sh
+++ net/batman-adv/files/lib/netifd/proto/batadv.sh
@@ -6,16 +6,19 @@ init_proto "$@"
 
 proto_batadv_init_config() {
 	proto_config_add_string "mesh"
+	proto_config_add_string "mesh_no_rebroadcast"
 }
 
 proto_batadv_setup() {
 	local config="$1"
 	local iface="$2"
 
-	local mesh
-	json_get_vars mesh
-
+	local mesh mesh_no_rebroadcast
+	json_get_vars mesh mesh_no_rebroadcast
+	
 	echo "$mesh" > "/sys/class/net/$iface/batman_adv/mesh_iface"
+	[ -n "$mesh_no_rebroadcast" ] && echo "$mesh_no_rebroadcast" > "/sys/class/net/$iface/batman_adv/no_rebroadcast"
+
 	proto_init_update "$iface" 1
 	proto_send_update "$config"
 }
